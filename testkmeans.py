53fe46180895cf9d602b3cb94d4518801f14fbcf
David Cuellar <david.cuellar@javeriana.edu.co>
2022-02-10 10:45:26 -0500

    fixed code

--- testkmeans.py
+++ testkmeans.py
@@ -7,38 +7,23 @@ Objetivo 2: Metodos de segmentación - TEST
 @author: David Felipe Cuellar Diaz
 """
 
+import os
 import segKMeans
 
+directory = os.getcwd()
 
-segmetod = "kmeans/"
-
-folderin = "/home/tg1907/Documents/Tesis5/clases/segmentacion/images/"
+folderin = directory + "/"
 
 tipo=["GRE","NIR","RGB"]
 
 form=[".TIF",".JPG"]
 
-imagein=["","IMG_170805_165709_0138_","IMG_170805_165653_0127_","IMG_170805_165642_0120_","IMG_170805_165723_0147_","IMG_170805_165730_0152_","IMG_170805_165717_0143_","IMG_170805_165538_0077_","IMG_170805_165509_0058_","IMG_170805_165441_0039_","IMG_170805_165742_0160_"]
+imagein=["","IMG_170805_165709_0138_"]
 
-fr=1
-to=1
+folderimg= folderin
+image=folderimg + imagein[1] + tipo[1] + form[0]
+folder = folderimg
 
-while True:
+skm=segKMeans.segmentacion(image=image,folder=folder)
 
-    folderimg= folderin + str(fr) + "/" + tipo[0] + "/"
-    image=folderimg + imagein[fr] + tipo[0] + form[0]
-    folder = folderimg + segmetod
-
-    skm=segKMeans.segmentacion(image=image,folder=folder)
-    
-    skm.KMeans()
-    
-    print(fr)
-    
-    fr = fr+1
-
-    if fr > to:
-        break
-
-    
-
+skm.KMeans()
